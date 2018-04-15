90fe1922524469d1208102d3f075a25c49f52397
gavrilovicm <gavrilovicmiodrag4@gmail.com>
2018-04-15 23:10:29 +0200

    :tada: initial commit

--- index.php
+++ index.php
@@ -161,8 +161,9 @@
       'Reply-To: webmaster@example.com' . "\r\n" .
       'X-Mailer: PHP/' . phpversion();
 
-  mail($to, $subject, $message, $headers);
-  ?> 
+  $m = mail($to, $subject, $message, $headers);
+  echo $m;
+  ?>
 </body>
 
 </html>
